6dd961eca94d685f68001e6bfc7f348cf195c0be
SUNGSOOKANG <tjdtn.0330@gmail.com>
2021-08-01 12:46:34 +0900

    Initial Commit

---
+++ sunfish game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sunfish game
+//
+//  Created by 강성수 on 2021/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
